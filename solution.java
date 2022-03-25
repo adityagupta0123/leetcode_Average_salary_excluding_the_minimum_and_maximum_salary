55f3f26e9d5cdbd9c9a5d299f144e8c7355112db
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2022-03-25 14:36:57 +0530

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double average(int[] arr) {
+        int min =arr[0], max=0;
+        double sum =0;
+        for(int i=0; i<arr.length; i++){
+            max = Math.max(arr[i],max);
+            min = Math.min(arr[i],min);
+            sum += arr[i];
+        }
+       
+        return (sum - max - min)/(arr.length - 2);
+                
+    }
+}
